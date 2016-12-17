392a1bf4bb3b6eabbb99c012019a586d9e4a2449
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-12-17 11:24:30 -0500

    Struct lab complete

--- StructLove/PizzaDeliveryService.swift
+++ StructLove/PizzaDeliveryService.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+struct PizzaDeliveryService {
+    
+    let location: Coordinate
+    var pizzasAvailable: Int = 10
+    
+    init(location: Coordinate) {
+        self.location = location
+    }
+    
+    func isInRange(to destination: Coordinate) -> Bool {
+        if self.location.distance(to: destination) <= 5000 {
+            return true
+        }
+        return false
+    }
+    
+    mutating func deliverPizza(to destination: Coordinate) -> Bool {
+        if isInRange(to: destination) && self.pizzasAvailable > 0 {
+            self.pizzasAvailable -= 1
+            return true
+        }
+        return false
+    }
+}
